8824307bee0bbd529a9a1e55a8b29d18bfc0829f
SrujanVBhat <srujanvbhat@gmail.com>
2023-04-30 17:40:28 +0530

    This is my first repo

---
+++ Srujan/src/LinearSearch.java
@@ -0,0 +1,13 @@
+public class LinearSearch{
+    public static void main(String[] args) {
+        int arr[][] = new int[3][];
+         arr[][] = {
+            {1,2,3}
+            {4,5,6}
+            {7,8,9}
+
+        }
+    }
+
+
+}
